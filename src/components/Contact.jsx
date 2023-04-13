0b0a7f37903db32c482a7fce531f277d01acf85a
rumbleroii <mrithik2010@gmail.com>
2023-04-13 12:07:40 +0530

    Footer Updated

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,25 +3,26 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <Container css={{
-        "backgroundColor": "$gray400",
-        "borderRadius": "10px",
-    }}>
-        <Text size={30} align="center">Want to Hear More, Contact Us</Text>
-        <Spacer y={2.5} />
-        <Container>
-            <Input clearable bordered labelPlaceholder="Name" initialValue="NextUI" />
-            <Spacer y={2.5} />
-            <Input
-                clearable
-                underlined
-                labelPlaceholder="Name"
-                initialValue="NextUI"
-            />
-            <Spacer y={1.5} />
-            <Input clearable label="Name" placeholder="Name" initialValue="NextUI" />
-        </Container>
-    </Container>
+    // <Container css={{
+    //     "backgroundColor": "$gray400",
+    //     "borderRadius": "10px",
+    // }}>
+    //     <Text size={30} align="center">Want to Hear More, Contact Us</Text>
+    //     <Spacer y={2.5} />
+    //     <Container>
+    //         <Input clearable bordered labelPlaceholder="Name" initialValue="NextUI" />
+    //         <Spacer y={2.5} />
+    //         <Input
+    //             clearable
+    //             underlined
+    //             labelPlaceholder="Name"
+    //             initialValue="NextUI"
+    //         />
+    //         <Spacer y={1.5} />
+    //         <Input clearable label="Name" placeholder="Name" initialValue="NextUI" />
+    //     </Container>
+    // </Container>
+    <></>
   )
 }
 
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,71 @@
 import React from 'react'
 
-import { Text } from '@nextui-org/react'
+import { Col, Container, Row, Text, Link} from '@nextui-org/react'
+
+// Icons
+import { HiOutlinePhone } from "react-icons/hi"
+import { HiOutlineCalendar } from "react-icons/hi"
+import { HiOutlineMail } from "react-icons/hi"
+import { HiLocationMarker } from "react-icons/hi"
+import { FaFacebookSquare } from "react-icons/fa"
+import { FaWhatsappSquare } from "react-icons/fa"
 
 const Footer = () => {
   return (
-    <div style={{
-        "backgroundColor": "black",
-        "height": "200px"
+    <Container fluid css={{
+      "backgroundColor": "black",
+      "padding":"5px"
     }}>
-        <Text color="white" align="center"> All Rights to the Company</Text>
-    </div>
+      <Row css={{
+        "justifyContent":"center"
+      }}>
+        <Text b color="white">All Rights Belong to NutrieDiet</Text>
+      </Row>
+      <Row css={{
+        "display":"flex",
+        "alignItems":"center",
+        "justifyContent":"center",
+        "gap":"3%",
+        "padding":"50px"
+      }}>
+        <div>
+          <Text color="white" size={30} b css={{"display":"flex", "alignItems":"center", "gap":"5px"}}><HiOutlineCalendar value={{ style: { verticalAlign: 'middle' } }}/>CONTACT US</Text>
+          <Text color="white" css={{"display":"flex", "alignItems":"center", "gap":"5px"}}><HiOutlinePhone value={{ style: { verticalAlign: 'middle' } }}/>+91 93914 50725</Text>
+          <Link color="white" css={{"display":"flex", "alignItems":"center", "gap":"5px"}}><HiOutlineMail value={{ style: { verticalAlign: 'middle' } }}/>nutriedietplan@gmail.com</Link>
+        </div>
+        <div style={{
+          "height":"130px",
+          "width": "2px",
+          "backgroundColor": "white"
+        }}>
+        </div>
+        <div>
+          <Text color="white" size={25} b css={{"display":"flex", "alignItems":"center", "gap":"5px"}}><HiLocationMarker value={{ style: { verticalAlign: 'middle' } }}/>OUR LOCATIONS</Text>
+          <div style={{
+            "width":"90%"
+          }}>
+            <Text color="white" size={20}>1. Nutrie Diet, Near Mayabazar Restaurant, Karkhana, Hyderabad - 500015</Text>
+            <Text color="white" size={20}>2. Image Hospital, Kavuri Hills, Madhapur, Hyderabad - 500081</Text>
+          </div>
+        </div>
+        <div style={{
+          "height":"130px",
+          "width": "2px",
+          "backgroundColor": "white"
+        }}>
+        </div>
+        <div>
+          <Text size={25} weight={"semibold"} color="white">Reach Out to us on: </Text>
+          <Text size={20} weight={"semibold"} color="white" css={{"display":"flex", "alignItems":"center", "gap":"5px"}}><FaFacebookSquare/>Facebook </Text>
+          <Text size={20} weight={"semibold"} color="white" css={{"display":"flex", "alignItems":"center", "gap":"5px"}}><FaWhatsappSquare/>WhatsApp</Text>
+        </div>
+      </Row>
+      <Row css={{
+        "justifyContent":"center"
+      }}>
+        <Text color="white">Made with Love, By Rithik ♥</Text>
+      </Row>
+    </Container>
   )
 }
 
